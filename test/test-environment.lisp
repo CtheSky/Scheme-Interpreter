bef02a93e41e8e3faa2427b65999a7ea1962bdbd
CtheSky <cthesky@yeah.net>
2017-01-14 22:27:40 +0800

    test: add test for environment

---
+++ test/test-environment.lisp
@@ -0,0 +1,24 @@
+(load "framework.lisp")
+(load "..//environment.lisp")
+
+(deftest test-environment ()
+  (let* ((inner (extend-environment '(a) '(0) *the-empty-environment*))
+	 (outer (extend-environment '(a b) '(1 2) inner)))
+    ;;check lookup-variable-value
+    (check 
+     (= (lookup-variable-value 'a inner) 0)
+     (= (lookup-variable-value 'a outer) 1)
+     (= (lookup-variable-value 'b outer) 2))
+   ;;check nset-variable-value
+    (nset-variable-value 'a 3 outer)
+    (check 
+      (= (lookup-variable-value 'a outer) 3)
+      (= (lookup-variable-value 'a inner) 0))
+   ;;check ndefine-variable
+    (ndefine-variable 'c 4 outer)
+    (check
+      (null (lookup-variable-value 'c inner))
+      (= (lookup-variable-value 'c outer)))))
+
+;; trigger test
+(test-environment)
