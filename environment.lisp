bc614003d8b41bdcfbf791d517df566fe3d89dd1
CtheSky <cthesky@yeah.net>
2017-01-16 22:12:01 +0800

    feat: add environment table

--- environment.lisp
+++ environment.lisp
@@ -1,22 +1,32 @@
-;;;; Environment is passed through eval and it does following things:
+;;;; Environment:
+;;;;  It is passed through eval and it does following things:
 ;;;;   1. maintain the value of variables
 ;;;;   2. support nested lexical scope
-;;;; 
-;;;; Public Interface:
+;;;;  Public Interface:
 ;;;;   1. lookup-variable-value (var env)
 ;;;;      returns value bound to <var> in the <env>, or signals an error if it is unbound
 ;;;;   2. extend-environment (vars vals base-env)
 ;;;;      returns a new environment bounding vars to values with base-env as enclosing environment
-;;;;   3. define-variable! (var val env)
+;;;;   3. ndefine-variable! (var val env)
 ;;;;      adds var bound to value in first frame
-;;;;   4. set-variable-value! (var val env)
+;;;;   4. nset-variable-value! (var val env)
 ;;;;      bound <var> in the <env> to <val>, or signals an error if the <var> is unbound
+;;;;  Implemetation : A list of Hashables, each represents a frame
 ;;;;
-;;;; Implemetation : A list of Hashables, each represents a frame
-
+;;;; An Environment table:
+;;;;  Why this?
+;;;;   When implementing function, it should be capable of telling which environment it has been defined
+;;;;   to ensure the corretness of scope rule. But since hashtable couldn't contains itself, so I need 
+;;;;   another way: function holds an symbol which maps to the environment, and all this mapping information
+;;;;   is stored in a table.
+;;;;  Public Interface:
+;;;;   5. nstore-environment (env)
+;;;;      returns a unique symbol which maps to <env> in the table
+;;;;   6. get-environment (symbol)
+;;;;      returns corresponding environment
 
 ;;; Helper functions and global variables
-
+(defparameter *environment-table* (make-hash-table))
 (defparameter *the-empty-environment* nil)
 (defun enclosing-environment (env) (cdr env))
 (defun first-frame (env) (car env))
@@ -57,3 +67,13 @@
       (when present
 	(setf (gethash var frame) val)
 	(return)))))
+
+(defun nstore-environment (env)
+  ;;returns a unique symbol which maps to <env> in the table
+  (let ((symbol (gensym)))
+    (setf (gethash symbol *environment-table*) env)
+    symbol))
+
+(defun get-environment (symbol)
+  ;;returns corresponding environment
+  (gethash symbol *environment-table*))
