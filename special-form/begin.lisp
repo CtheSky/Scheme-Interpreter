d0b990b234569600a417ac39c78c2cd54ac56e34
CtheSky <cthesky@yeah.net>
2017-01-18 12:19:45 +0800

    feat: add sequence->exp function

--- special-form/begin.lisp
+++ special-form/begin.lisp
@@ -5,6 +5,11 @@
 (defun first-exp (seq) (car seq))
 (defun rest-exps (seq) (cdr seq))
 (defun make-begin (seq) (cons 'begin seq))
+(defun sequence->exp (seq)
+  ;; returns an expression representing given sequence, enclose with begin if needed
+  (cond ((null seq) seq)
+	((last-exp-p seq) (first-exp seq))
+	(t (make-begin seq))))
 
 (defun analyze-begin (exp)
   (analyze-sequence (begin-actions exp)))
