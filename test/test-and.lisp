f0997053d684c572c77ca3a0844039763e6feb17
CtheSky <cthesky@yeah.net>
2017-01-23 20:08:30 +0800

    test: add test for and

---
+++ test/test-and.lisp
@@ -0,0 +1,20 @@
+(load "eval.lisp")
+(load ".//test//framework.lisp")
+
+(deftest test-and ()
+  (let ((env (extend-environment '() '() *env*)))
+    ;;check no exp
+    (check (equal t (meval '(and) env)))
+    ;;check single exp
+    (check (equal t (meval '(and true) env)))
+    (check (equal nil (meval '(and false) env)))
+    ;;check combination
+    (check (equal t (meval '(and true true) env)))
+    (check (equal nil (meval '(and true false) env)))
+    ;;check short circuit
+    (check (equal nil (meval '(and (define a 1) false (define b 0)) env)))
+    (check (= 1 (meval 'a env)))
+    (check (null (meval 'b env)))))
+
+;; trigger test
+(test-and)
